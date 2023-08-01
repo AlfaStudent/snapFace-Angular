038e4098a2298653e5f2545763ca15470f409d1d
Alpha CONDE <abdoul.conde99@gmail.com>
2023-08-01 11:24:52 +0200

    click event management

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  url!: string;
+
+  ngOnInit() {
+    this.title = "archibalde";
+    this.description = "mon meilleur ami depuis 3 ans";
+    this.createDate = new Date();
+    this.snaps = 6;
+    this.url = "https://eu.366concept.com/cdn/shop/products/36JACSV5202NN_448a988e-1c39-49bf-98cd-2a49858feb1c_900x.jpg?v=1667648539"
+
+  }
+
+  onAddSnap() {
+    this.snaps++;
+  }
+
+
+}
