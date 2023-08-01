eb07fe1704d7e367677a5e48c29b5519134d01df
Alpha CONDE <abdoul.conde99@gmail.com>
2023-08-01 13:58:23 +0200

    button change event

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,6 +11,8 @@ export class FaceSnapComponent implements OnInit {
   createDate!: Date;
   snaps!: number;
   url!: string;
+  buttonText!: string;
+  anabled! : boolean;
 
   ngOnInit() {
     this.title = "archibalde";
@@ -18,12 +20,19 @@ export class FaceSnapComponent implements OnInit {
     this.createDate = new Date();
     this.snaps = 6;
     this.url = "https://eu.366concept.com/cdn/shop/products/36JACSV5202NN_448a988e-1c39-49bf-98cd-2a49858feb1c_900x.jpg?v=1667648539"
+    this.buttonText = "oh Snap";
+    this.anabled = true;
 
   }
 
-  onAddSnap() {
-    this.snaps++;
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
   }
-
 
 }
