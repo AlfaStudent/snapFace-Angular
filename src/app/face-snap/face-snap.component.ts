83048a6db60ff349996e2bd270474e9c1a46d1da
Alpha CONDE <abdoul.conde99@gmail.com>
2023-08-07 11:14:12 +0200

    add a model for a component

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,36 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createDate!: Date;
-  snaps!: number;
-  url!: string;
+  @Input() faceSnap!: FaceSnap
+  // title!: string;
+  // description!: string;
+  // createDate!: Date;
+  // snaps!: number;
+  // url!: string;
   buttonText!: string;
-  anabled! : boolean;
+  // anabled! : boolean;
 
   ngOnInit() {
-    this.title = "archibalde";
-    this.description = "mon meilleur ami depuis 3 ans";
-    this.createDate = new Date();
-    this.snaps = 6;
-    this.url = "https://eu.366concept.com/cdn/shop/products/36JACSV5202NN_448a988e-1c39-49bf-98cd-2a49858feb1c_900x.jpg?v=1667648539"
-    this.buttonText = "oh Snap";
-    this.anabled = true;
+    // this.title = "archibalde";
+    // this.description = "mon meilleur ami depuis 3 ans";
+    // this.createDate = new Date();
+    // this.snaps = 6;
+    // this.url = "https://eu.366concept.com/cdn/shop/products/36JACSV5202NN_448a988e-1c39-49bf-98cd-2a49858feb1c_900x.jpg?v=1667648539"
+    this.buttonText = "Oh Snap!";
+    // this.anabled = true;
 
   }
 
   onSnap() {
     if (this.buttonText === 'Oh Snap!') {
-      this.snaps++;
-      this.buttonText = 'Oops, unSnap!';
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oops, Snap!';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = 'Oh Snap!';
     }
   }
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,15 @@
+export class FaceSnap {
+
+
+    constructor(public title: string, 
+                public description: string, 
+                public url: string,
+                public createDate: Date, 
+                public snaps: number,
+                public location?: string){
+
+
+        
+    }
+
+}
