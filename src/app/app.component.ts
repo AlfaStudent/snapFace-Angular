397fb99e3df217fbadfb6020afbce625238a5d37
Alpha CONDE <abdoul.conde99@gmail.com>
2023-08-07 14:41:35 +0200

    add location in objectgit add *

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,13 @@ import { FaceSnap } from './models/face-snap.model';
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap;
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
+    this.mySnap = {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      url: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createDate: new Date(),
+      snaps: 0,
+      location: "Paris"
+    };
   }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,15 +1,22 @@
 export class FaceSnap {
+    public title!: string;
+    public description!: string;
+    public url!: string;
+    public createDate!: Date;
+    public snaps!: number;
+    public location?: string;
 
 
-    constructor(public title: string, 
-                public description: string, 
-                public url: string,
-                public createDate: Date, 
-                public snaps: number,
-                public location?: string){
+
+    // constructor(public title: string, 
+    //             public description: string, 
+    //             public url: string,
+    //             public createDate: Date, 
+    //             public snaps: number,
+    //             public location?: string){
 
 
         
-    }
+    // }
 
 }
