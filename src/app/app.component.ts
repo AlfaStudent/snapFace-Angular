e81669c5cbecac0954bc48763ac43d181d69b95c
Alpha CONDE <abdoul.conde99@gmail.com>
2023-08-11 14:19:09 +0200

    tabulate data with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,65 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
+  // mySnap!: FaceSnap;
+  // autherSnap!: FaceSnap;
+  // anAutherSnap!: FaceSnap;
   ngOnInit() {
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        url: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createDate: new Date(),
+        snaps: 0,
+        location: "Paris"
+      },
+      {
+
+      title: 'Archibald2',
+      description: 'Mon meilleur ami depuis tout petit et encore tout petit !',
       url: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       createDate: new Date(),
       snaps: 0,
-      location: "Paris"
-    };
+      location: "Orleans"
+      },
+
+      {
+
+      title: 'Archibald3',
+      description: 'Mon meilleur ami depuis tout petit et encore tout petit !',
+      url: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createDate: new Date(),
+      snaps: 0,
+      }
+
+
+    ]
+    // this.mySnap = {
+    //   title: 'Archibald',
+    //   description: 'Mon meilleur ami depuis tout petit !',
+    //   url: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+    //   createDate: new Date(),
+    //   snaps: 0,
+    //   location: "Paris"
+    // };
+    // this.autherSnap = {
+    //   title: 'Archibald2',
+    //   description: 'Mon meilleur ami depuis tout petit et encore tout petit !',
+    //   url: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+    //   createDate: new Date(),
+    //   snaps: 0,
+    //   location: "Orleans"
+    // };
+    // this.anAutherSnap = {
+    //   title: 'Archibald3',
+    //   description: 'Mon meilleur ami depuis tout petit et encore tout petit !',
+    //   url: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+    //   createDate: new Date(),
+    //   snaps: 0,
+      
+    // };
+
   }
 }
