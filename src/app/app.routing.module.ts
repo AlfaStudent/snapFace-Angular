4d868c1b78663ffe1db5976765db957f1352a4e2
Alpha CONDE <abdoul.conde99@gmail.com>
2023-08-25 15:30:27 +0200

    others routes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FaceSnapComponent } from "./face-snap/face-snap.component";
+import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 const routes: Routes = [
-    { path: 'oui', component: FaceSnapComponent }
+    { path: 'facesnaps', component: FaceSnapListComponent },
+    {path: "", component: LandingPageComponent}
 
 
 ];
