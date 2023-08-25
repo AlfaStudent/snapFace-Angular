8777c2851e4a40a7f8ba6dd954170017eeceef25
Alpha CONDE <abdoul.conde99@gmail.com>
2023-08-25 14:07:25 +0200

    configuration of routing

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FaceSnapComponent } from "./face-snap/face-snap.component";
+const routes: Routes = [
+    { path: 'oui', component: FaceSnapComponent }
+
+
+];
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+
+}
